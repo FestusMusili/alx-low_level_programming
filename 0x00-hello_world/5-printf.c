54aa5f126a3356fdd584cb2f0144696a6310916e
FestusMusili <fessycompass@gmail.com>
2023-12-14 13:34:06 -0800

    printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+* main - A program that print a line using printf function
+* Return: 0 (Success)
+*/
+
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of arti,\n")
+return (0);
+}
+root@a270ef321e71:~/alx-low_level_programming/0x00-hello_world#
