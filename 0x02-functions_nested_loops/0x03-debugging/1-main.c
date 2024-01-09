5063d64fe5932089e2c8e6c4fcfb187a52f36dfe
FestusMusili <fessycompass@gmail.com>
2024-01-09 14:35:03 -0800

    1-main.c

---
+++ 0x02-functions_nested_loops/0x03-debugging/1-main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+* main - Entry point of the program
+*
+*
+* Description: This function prints messages to indicate an infinite loop.
+*
+*
+* Return: 0 (Success)
+*/
+
+int main(void)
+
+{
+
+/* Print a message indicating an incoming infinite loop */
+printf("Infinite loop incoming :(\n");
+
+/* Print a message indicating that the infinite loop is avoided */
+printf("Infinite loop avoided! \\o/\n");
+
+return (0);
