4b63dab71f33541924c25b3e7f8d9475b00bc92b
FestusMusili <fessycompass@gmail.com>
2024-04-13 19:34:06 -0700

    0x1A-hash_tables

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,38 +1,29 @@
 #include "hash_tables.h"
-#include <stdlib.h>
 
 /**
- * hash_table_create - creates a hash table
- * @size: size of the array
- * Returns: pointer to the created hash table
+ * hash_table_create - This creates a hash table
+ * @size: is the size of the array
+ *
+ * Return: a pointer to the newly created hash table
  */
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *new_table;
-	unsigned long int i;
+hash_table_t *table;
 
-	if (size == NULL)
-		return (NULL);
+if (size == 0)
+return (NULL);
 
-	/* Allocate memory for the hasg table structure */
-	new_table = malloc(sizeof(hash_table_t));
-	if (new_table == NULL)
-		return (NULL);
+table = calloc(1, sizeof(hash_table_t));
+if (table == NULL)
+return (NULL);
 
-	/* Allocate memory for the array of pointers to hash nodes */
-	new_table->ptr_array = malloc(sizeof(hash_node_t *) * size);
-	if (new_table->ptr_array == NULL)
-	{
-		free(new_table);
-		return (NULL);
-	}
-
-	new_table->size = size;
-
-	/* initialize each element of the array to NULL */
-	for (i = 0; i < size; i++)
-		new_table->ptr_array[i] = NULL;
-
-	return (new_table);
+table->size = size;
+table->array = calloc(size, sizeof(hash_node_t));
+if (table->array == NULL)
+{
+free(table);
+return (NULL);
+}
+return (table);
 }
